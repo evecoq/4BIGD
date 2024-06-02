d912ac649e4a2161e57f25bc7284dfcb874b0cd1
Rotabey <jbaptiste.cherrier.pro@gmail.com>
2024-06-02 23:09:17 +0200

    hdfs

--- JOUR5/exo.r
+++ JOUR5/exo.r
@@ -55,21 +55,3 @@ df <- df %>% mutate(across(where(is.character), trimws))
 
 #Export dataset as csv
 write.csv(df, "imdb_dataset.csv", row.names=FALSE)
-
-library(rhdfs)
-
-# Connect to the HDFS file system
-hdfs.init()
-
-# Destination path on HDFS
-destination_path <- "/user/hadoop/imdb_dataset"
-
-# Check if the directory already exists on HDFS
-if (!hdfs.exists(destination_path)) {
-  # Create the directory if it doesn't exist
-  hdfs.mkdir(destination_path, recursive = TRUE)
-  cat("Directory created successfully on HDFS.\n")
-}
-
-# Copy the CSV file to HDFS
-hdfs.put("imdb_dataset.csv", paste0(destination_path, "imdb_dataset.csv"))
---
+++ JOUR5/hdfs.R
@@ -0,0 +1,17 @@
+library(rhdfs)
+
+# Connect to the HDFS file system
+hdfs.init()
+
+# Destination path on HDFS
+destination_path <- "/user/hadoop/imdb_dataset"
+
+# Check if the directory already exists on HDFS
+if (!hdfs.exists(destination_path)) {
+  # Create the directory if it doesn't exist
+  hdfs.mkdir(destination_path, recursive = TRUE)
+  cat("Directory created successfully on HDFS.\n")
+}
+
+# Copy the CSV file to HDFS
+hdfs.put("imdb_dataset.csv", paste0(destination_path, "imdb_dataset.csv"))
